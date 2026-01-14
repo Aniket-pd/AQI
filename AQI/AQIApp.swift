39981e1beee70e9839ec0cf67d2b8d64828e5e8b
Aniket-pd <aniketprasad123@gmail.com>
2026-01-15 00:38:44 +0530

    Initial Commit

---
+++ AQI/AQIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AQIApp.swift
+//  AQI
+//
+//  Created by Aniket prasad on 15/1/26.
+//
+
+import SwiftUI
+
+@main
+struct AQIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AQI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AQI
+//
+//  Created by Aniket prasad on 15/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
